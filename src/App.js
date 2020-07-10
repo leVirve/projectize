fe97e09a7e2f5a092c32b3c98374963926f08798
salas <gae.m.project@gmail.com>
2020-07-11 00:44:36 +0800

    update for metadata and fonts
    Former-commit-id: db6462a32697425c0a84f52802b03fadb3dab12a

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ import paperThumbImage from './images/thumb.jpg';
 
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
 import Divider from '@material-ui/core/Divider';
-import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import Header from './components/Headers.jsx';
 import Author from './components/Author.jsx';
 import Teaser from './components/Teaser.jsx';
@@ -18,6 +17,7 @@ import Teaser from './components/Teaser.jsx';
 const useStyles = makeStyles((theme) => ({
   titleHead: {
     padding: theme.spacing(8, 0, 6),
+    fontFamily: 'Source Serif Pro',
   },
 }));
 
@@ -30,11 +30,11 @@ const sections = [
   { title: 'References', url: '#' },
 ];
 const authors = [
-  { name: 'Yu-Lun Liu', url: 'http://www.cmlab.csie.ntu.edu.tw/~yulunliu/', affiliation: '1,5'},
-  { name: 'Wei-Sheng Lai', url: '', affiliation: '2'},
-  { name: 'Ming-Hsuan Yang', url: '', affiliation: '2,4'},
-  { name: 'Yung-Yu Chuang', url: '', affiliation: '1'},
-  { name: 'Jia-Bin Huang', url: '', affiliation: '3'},
+  { name: 'Yu-Lun Liu', url: 'http://www.cmlab.csie.ntu.edu.tw/~yulunliu/', affiliation: '1,5' },
+  { name: 'Wei-Sheng Lai', url: '', affiliation: '2' },
+  { name: 'Ming-Hsuan Yang', url: '', affiliation: '2,4' },
+  { name: 'Yung-Yu Chuang', url: '', affiliation: '1' },
+  { name: 'Jia-Bin Huang', url: '', affiliation: '3' },
 ]
 const affiliations = [
   { number: '1', name: 'National Taiwan University', url: '' },
@@ -44,36 +44,56 @@ const affiliations = [
   { number: '5', name: 'MediaTek Inc.', url: '' },
 ]
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Source Sans Pro',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
 
 function App() {
   const classes = useStyles();
 
   return (
-    <div className="App">
-      <Container maxWidth="lg">
-        <Header title="Research" sections={sections} />
-        <Typography variant="h3" gutterBottom className={classes.titleHead}>
-          Learning to See Through Obstructions
-        </Typography>
-        <Author authors={authors} affiliations={affiliations}/>
-        <Teaser imageSrc={teaserImage} />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <Container maxWidth="lg">
+          <Header title="Research" sections={sections} />
+          <Typography variant="h3" gutterBottom className={classes.titleHead}>
+            Learning to See Through Obstructions
+          </Typography>
+          <Author authors={authors} affiliations={affiliations} />
+          <Teaser imageSrc={teaserImage} />
 
-        <Typography id="abstract" align="left" variant="h4" color="inherit" gutterBottom>
-          Abstract
+          <Typography id="abstract" align="left" variant="h4" color="inherit" gutterBottom>
+            Abstract
         </Typography>
-        <Divider />
-        <Typography variant="h6" align="left" paragraph>
-          We present a learning-based approach for removing unwanted obstructions, such as window reflections, fence occlusions or raindrops, from a short sequence of images captured by a moving camera. Our method leverages the motion differences between the background and the obstructing elements to recover both layers. Specifically, we alternate between estimating dense optical flow fields of the two layers and reconstructing each layer from the flow-warped images via a deep convolutional neural network. The learning-based layer reconstruction allows us to accommodate potential errors in the flow estimation and brittle assumptions such as brightness consistency. We show that training on synthetically generated data transfers well to real images. Our results on numerous challenging scenarios of reflection and fence removal demonstrate the effectiveness of the proposed method.
+          <Divider />
+          <Typography variant="h6" align="left" paragraph>
+            We present a learning-based approach for removing unwanted obstructions, such as window reflections, fence occlusions or raindrops, from a short sequence of images captured by a moving camera. Our method leverages the motion differences between the background and the obstructing elements to recover both layers. Specifically, we alternate between estimating dense optical flow fields of the two layers and reconstructing each layer from the flow-warped images via a deep convolutional neural network. The learning-based layer reconstruction allows us to accommodate potential errors in the flow estimation and brittle assumptions such as brightness consistency. We show that training on synthetically generated data transfers well to real images. Our results on numerous challenging scenarios of reflection and fence removal demonstrate the effectiveness of the proposed method.
         </Typography>
 
-        <Typography id="abstract" align="left" variant="h4" color="inherit" gutterBottom>
-          Paper
+          <Typography id="abstract" align="left" variant="h4" color="inherit" gutterBottom>
+            Paper
         </Typography>
-        <Divider />
-        <Teaser imageSrc={paperThumbImage} />
+          <Divider />
+          <Teaser imageSrc={paperThumbImage} />
 
-      </Container>
-    </div>
+        </Container>
+      </div>
+    </ThemeProvider>
   );
 }
 
